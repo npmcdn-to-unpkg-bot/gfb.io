66bd6e14e47367b998f272c63ac8be833d999352
INORIA <inoria@goldfish-bowl.jp>
2016-08-25 18:47:41 +0900

    Adjust for github pages

---
+++ html-generators/gulpfile-prod.babel.js
@@ -0,0 +1,39 @@
+import gulp from 'gulp';
+import webpack from 'webpack-stream';
+import browserSync from 'browser-sync';
+import postcss from 'gulp-postcss';
+import plumber from 'gulp-plumber';
+import notify from 'gulp-notify';
+
+import webpackOptions from './webpack-prod.config.babel.js';
+import browserSyncOptions from './bs-config.js';
+
+const paths = {
+  dist: '../docs/html-generators',
+  src: 'src',
+};
+
+gulp.task('css', () => gulp.src('src/styles/styles.css')
+  .pipe(postcss([
+    require('postcss-import'),
+    require('postcss-nested'),
+    require('autoprefixer')({ browsers: ['last 2 versions'] }),
+  ]))
+  .pipe(gulp.dest(paths.dist + '/styles')));
+
+gulp.task('html-copy', () => {
+  return gulp.src(`${paths.src}/**/*.html`)
+    .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('webpack', () => {
+  return gulp.src(`${paths.src}/**/*.js`)
+    .pipe(plumber({
+      errorHandler: notify.onError('Error: <%= error.message %>'),
+    }))
+    .pipe(webpack(webpackOptions))
+    .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('default', gulp.parallel('webpack', 'html-copy', 'css'));
+
